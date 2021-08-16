93302cf82d1557506654d29082de7c558a17b112
JoniMeichtry11 <joni.meichtry@gmail.com>
2021-08-15 23:25:57 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,317 @@
+const div = document.querySelector(".contenedor-modal");
+
+div.addEventListener('click', function () {
+    div.classList.toggle('active');
+})
+
+const div2 = document.querySelector(".contenedor-modal2");
+
+div2.addEventListener('click', function () {
+    div2.classList.toggle('active');
+})
+
+const div3 = document.querySelector(".contenedor-modal3");
+
+div3.addEventListener('click', function () {
+    div3.classList.toggle('active');
+})
+
+const div4 = document.querySelector(".contenedor-modal4");
+
+div4.addEventListener('click', function () {
+    div4.classList.toggle('active');
+})
+
+const div5 = document.querySelector(".contenedor-modal5");
+
+div5.addEventListener('click', function () {
+    div5.classList.toggle('active');
+})
+
+const div6 = document.querySelector(".contenedor-modal6");
+
+div6.addEventListener('click', function () {
+    div6.classList.toggle('active');
+})
+
+const div7 = document.querySelector(".contenedor-modal7");
+
+div7.addEventListener('click', function () {
+    div7.classList.toggle('active');
+})
+
+const div8 = document.querySelector(".contenedor-modal8");
+
+div8.addEventListener('click', function () {
+    div8.classList.toggle('active');
+})
+
+const div9 = document.querySelector(".contenedor-modal9");
+
+div9.addEventListener('click', function () {
+    div9.classList.toggle('active');
+})
+
+const div10 = document.querySelector(".contenedor-modal10");
+
+div10.addEventListener('click', function () {
+    div10.classList.toggle('active');
+})
+
+const div11 = document.querySelector(".contenedor-modal11");
+
+div11.addEventListener('click', function () {
+    div11.classList.toggle('active');
+})
+
+const div12 = document.querySelector(".contenedor-modal12");
+
+div12.addEventListener('click', function () {
+    div12.classList.toggle('active');
+})
+
+const div13 = document.querySelector(".contenedor-modal13");
+
+div13.addEventListener('click', function () {
+    div13.classList.toggle('active');
+})
+
+const div14 = document.querySelector(".contenedor-modal14");
+
+div14.addEventListener('click', function () {
+    div14.classList.toggle('active');
+})
+
+const div15 = document.querySelector(".contenedor-modal15");
+
+div15.addEventListener('click', function () {
+    div15.classList.toggle('active');
+})
+
+const div16 = document.querySelector(".contenedor-modal16");
+
+div16.addEventListener('click', function () {
+    div16.classList.toggle('active');
+})
+
+const div17 = document.querySelector(".contenedor-modal17");
+
+div17.addEventListener('click', function () {
+    div17.classList.toggle('active');
+})
+
+const div18 = document.querySelector(".contenedor-modal18");
+
+div18.addEventListener('click', function () {
+    div18.classList.toggle('active');
+})
+
+const div19 = document.querySelector(".contenedor-modal19");
+
+div19.addEventListener('click', function () {
+    div19.classList.toggle('active');
+})
+
+const div20 = document.querySelector(".contenedor-modal20");
+
+div20.addEventListener('click', function () {
+    div20.classList.toggle('active');
+})
+
+const div21 = document.querySelector(".contenedor-modal21");
+
+div21.addEventListener('click', function () {
+    div21.classList.toggle('active');
+})
+
+const div22 = document.querySelector(".contenedor-modal22");
+
+div22.addEventListener('click', function () {
+    div22.classList.toggle('active');
+})
+
+const div23 = document.querySelector(".contenedor-modal23");
+
+div23.addEventListener('click', function () {
+    div23.classList.toggle('active');
+})
+
+const div24 = document.querySelector(".contenedor-modal24");
+
+div24.addEventListener('click', function () {
+    div24.classList.toggle('active');
+})
+
+const div25 = document.querySelector(".contenedor-modal25");
+
+div25.addEventListener('click', function () {
+    div25.classList.toggle('active');
+})
+
+const div26 = document.querySelector(".contenedor-modal26");
+
+div26.addEventListener('click', function () {
+    div26.classList.toggle('active');
+})
+
+const div27 = document.querySelector(".contenedor-modal27");
+
+div27.addEventListener('click', function () {
+    div27.classList.toggle('active');
+})
+
+const div28 = document.querySelector(".contenedor-modal28");
+
+div28.addEventListener('click', function () {
+    div28.classList.toggle('active');
+})
+
+const div29 = document.querySelector(".contenedor-modal29");
+
+div29.addEventListener('click', function () {
+    div29.classList.toggle('active');
+})
+
+const div30 = document.querySelector(".contenedor-modal30");
+
+div30.addEventListener('click', function () {
+    div30.classList.toggle('active');
+})
+
+const div31 = document.querySelector(".contenedor-modal31");
+
+div31.addEventListener('click', function () {
+    div31.classList.toggle('active');
+})
+
+const div32 = document.querySelector(".contenedor-modal32");
+
+div32.addEventListener('click', function () {
+    div32.classList.toggle('active');
+})
+
+const div33 = document.querySelector(".contenedor-modal33");
+
+div33.addEventListener('click', function () {
+    div33.classList.toggle('active');
+})
+
+const div34 = document.querySelector(".contenedor-modal34");
+
+div34.addEventListener('click', function () {
+    div34.classList.toggle('active');
+})
+
+const div35 = document.querySelector(".contenedor-modal35");
+
+div35.addEventListener('click', function () {
+    div35.classList.toggle('active');
+})
+
+const div36 = document.querySelector(".contenedor-modal36");
+
+div36.addEventListener('click', function () {
+    div36.classList.toggle('active');
+})
+
+const div37 = document.querySelector(".contenedor-modal37");
+
+div37.addEventListener('click', function () {
+    div37.classList.toggle('active');
+})
+
+const div38 = document.querySelector(".contenedor-modal38");
+
+div38.addEventListener('click', function () {
+    div38.classList.toggle('active');
+})
+
+const div39 = document.querySelector(".contenedor-modal39");
+
+div39.addEventListener('click', function () {
+    div39.classList.toggle('active');
+})
+
+const div40 = document.querySelector(".contenedor-modal40");
+
+div40.addEventListener('click', function () {
+    div40.classList.toggle('active');
+})
+
+const div41 = document.querySelector(".contenedor-modal41");
+
+div41.addEventListener('click', function () {
+    div41.classList.toggle('active');
+})
+
+const div42 = document.querySelector(".contenedor-modal42");
+
+div42.addEventListener('click', function () {
+    div42.classList.toggle('active');
+})
+
+const div43 = document.querySelector(".contenedor-modal43");
+
+div43.addEventListener('click', function () {
+    div43.classList.toggle('active');
+})
+
+const div44 = document.querySelector(".contenedor-modal44");
+
+div44.addEventListener('click', function () {
+    div44.classList.toggle('active');
+})
+
+const div45 = document.querySelector(".contenedor-modal45");
+
+div45.addEventListener('click', function () {
+    div45.classList.toggle('active');
+})
+
+const div46 = document.querySelector(".contenedor-modal46");
+
+div46.addEventListener('click', function () {
+    div46.classList.toggle('active');
+})
+
+const div47 = document.querySelector(".contenedor-modal47");
+
+div47.addEventListener('click', function () {
+    div47.classList.toggle('active');
+})
+
+const div48 = document.querySelector(".contenedor-modal48");
+
+div48.addEventListener('click', function () {
+    div48.classList.toggle('active');
+})
+
+const div49 = document.querySelector(".contenedor-modal49");
+
+div49.addEventListener('click', function () {
+    div49.classList.toggle('active');
+})
+
+const div50 = document.querySelector(".contenedor-modal50");
+
+div50.addEventListener('click', function () {
+    div50.classList.toggle('active');
+})
+
+const div51 = document.querySelector(".contenedor-modal51");
+
+div51.addEventListener('click', function () {
+    div51.classList.toggle('active');
+})
+
+const div52 = document.querySelector(".contenedor-modal52");
+
+div52.addEventListener('click', function () {
+    div52.classList.toggle('active');
+})
+
+const div53 = document.querySelector(".contenedor-modal53");
+
+div53.addEventListener('click', function () {
+    div53.classList.toggle('active');
+})
